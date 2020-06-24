e4e79d272132949c5a4942c87e4727eb0c413c65
EugeneMints <eugene.minets@gmail.com>
2020-06-25 01:18:09 +0300

    updated app.properties

--- src/main/java/telran/propetszuul/PropetszuulApplication.java
+++ src/main/java/telran/propetszuul/PropetszuulApplication.java
@@ -2,12 +2,22 @@ package telran.propetszuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import telran.propetszuul.filters.SimpleFilter;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class PropetszuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PropetszuulApplication.class, args);
 	}
+	
+	  @Bean
+	  public SimpleFilter simpleFilter() {
+	    return new SimpleFilter();
+	  }
 
 }
