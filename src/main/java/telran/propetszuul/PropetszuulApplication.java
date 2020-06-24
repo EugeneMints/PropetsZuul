3fdf3b6573e76a6523b9f72fc82cc27c722d9b69
EugeneMints <eugene.minets@gmail.com>
2020-06-25 01:05:22 +0300

    first commit

---
+++ src/main/java/telran/propetszuul/PropetszuulApplication.java
@@ -0,0 +1,13 @@
+package telran.propetszuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropetszuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropetszuulApplication.class, args);
+	}
+
+}
